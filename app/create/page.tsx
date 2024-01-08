c96c553ca1571d6fb92cdae4c1ac94dccef6c6cc
Gökmen <gkmenozkn@gmail.com>
2024-01-08 20:44:01 +0300

    ui: show toast when creating a user

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import { FormData } from '@/types/User';
 import UserApi from '@/utils/api';
 import FormView from '@/components/FormView';
+import { toast, ToastContainer } from 'react-toastify';
 
 const userApi = new UserApi();
 
@@ -33,21 +34,45 @@ export default function Create() {
       const response = await userApi.createUser(formData);
 
       console.log('success:', response);
-      router.push('/');
+
+      // show success message
+      toast.success('User created successfully!', {
+        position: 'top-center',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored',
+      });
+
+      // redirect to home page after a second
+      setTimeout(() => {
+        router.push('/');
+      }, 1000);
     } catch (error) {
+      toast.error('Something went wrong!', {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 3000,
+        closeOnClick: true,
+        theme: 'colored',
+      });
       console.log('Error:', error);
     }
   };
 
   return (
     <>
+      <ToastContainer />
+
       {/* Back button */}
       <div className='py-4 max-w-2xl mx-auto'>
         <Link href='/' className='text-blue-600 hover:text-blue-400'>
           {'<'} Back
         </Link>
       </div>
-      
+
       <FormView
         handleChange={handleChange}
         handleSubmit={handleSubmit}
--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,6 @@ import UserList from '@/components/UserList';
 
 export default function Home() {
 
-  // useEffect(() => {
-  //   axios
-  //     .get('http://localhost:8080/users')
-  //     .then((res) => {
-  //       console.log(res);
-  //       setData(res.data);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
-
   return (
     <>
       <div className='mb-4'></div>
